b7903c18760221f09296847616c44729d96ed233
jakehrr <jthartnoll@yahoo.co.uk>
2025-04-28 17:25:29 +0100

    most of the logic created

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Security.Cryptography;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class GameManager : MonoBehaviour
     public GameObject player2Text;
     public TextMeshProUGUI[] player1scores;
     public TextMeshProUGUI[] player2scores;
+    public TextMeshProUGUI mainPlayer1score;
+    public TextMeshProUGUI mainPlayer2score;
 
     [Header("Script Accessing")]
     public HorizontalPower horizontalPowerScript;
@@ -24,10 +27,13 @@ public class GameManager : MonoBehaviour
     public bool isPlayer1sTurn = true;
     public int player1ScoreIndex;
     public int player2ScoreIndex;
+    public int currentPlayer1Score;
+    public int currentPlayer2Score;
 
     [Header("Dartboard Info")]
     public RectTransform dartboard;
     public RectTransform dart;
+    public RectTransform centrePoint;
 
     public void BeginGame()
     {
@@ -60,6 +66,16 @@ public class GameManager : MonoBehaviour
         BeginHorizontalPower();
     }
 
+   /* private void Update()
+    {
+        Vector2 centrePointPosition = centrePoint.position;
+        Vector2 dartPosition = dart.anchoredPosition;
+
+        float distanceFromCentre = Vector2.Distance(centrePointPosition, dartPosition);
+
+        Debug.Log(distanceFromCentre);
+    }*/
+
     public void PlaceDart()
     {
         Vector2 dartboardSize = dartboard.rect.size;
@@ -69,36 +85,74 @@ public class GameManager : MonoBehaviour
 
         dart.anchoredPosition = new Vector2(dartPosX, dartPosY);
 
-        // Calculator Angle Test
+        // Get Landing Score (Un-Multiplied)
         int sectionHit = GetComponent<DartBoardHitCalculator>().GetDartboardSection(dart.anchoredPosition);
+
+        // Get the distance from the centre point for multiplier calculations
+        Vector2 centrePointPosition = centrePoint.position;
+        Vector2 dartPosition = dart.anchoredPosition;
+        float distanceFromCentre = Vector2.Distance(centrePointPosition, dartPosition);
+        
+        // Base Multiplier
+        int multiplier = 1;
+        if(distanceFromCentre >= 249 && distanceFromCentre <= 287) // Triple Score Multiplier
+        {
+            multiplier = 3;
+        }
+        else if(distanceFromCentre >= 409f && distanceFromCentre <= 445f) // Double Score Multiplier
+        {
+            multiplier = 2;
+        }
+        else if(distanceFromCentre <= 38) // Bullseye Hit
+        {
+            sectionHit = 50;
+        }
+        else if(distanceFromCentre >= 38.01 && distanceFromCentre <= 72) // Secondary Bullseye Hit
+        {
+            sectionHit = 25;
+        }
 
         if (isPlayer1sTurn)
         {
-            player1Text.SetActive(true);
-            player2Text.SetActive(false);
-
             if(player1ScoreIndex < player1scores.Length)
             {
                 player1scores[player1ScoreIndex].gameObject.SetActive(true);
+                sectionHit *= multiplier;
                 player1scores[player1ScoreIndex].text = sectionHit.ToString();
+                currentPlayer1Score += sectionHit;
+                mainPlayer1score.text = currentPlayer1Score.ToString();
                 player1ScoreIndex++;
             }
         }
         else if(!isPlayer1sTurn)
         {
-            player1Text.SetActive(false);
-            player2Text.SetActive(true);
-
             if (player2ScoreIndex < player2scores.Length)
             {
                 player2scores[player2ScoreIndex].gameObject.SetActive(true);
+                sectionHit *= multiplier;
                 player2scores[player2ScoreIndex].text = sectionHit.ToString();
+                currentPlayer2Score += sectionHit;;
+                mainPlayer2score.text = currentPlayer2Score.ToString();
                 player2ScoreIndex++;
             }
         }
         isPlayer1sTurn = !isPlayer1sTurn;
 
         StartCoroutine(BeginThrowReset());
+    }
+
+    private void DisplayTextUserChange()
+    {
+        if (isPlayer1sTurn)
+        {
+            player1Text.SetActive(true);
+            player2Text.SetActive(false);
+        }
+        else if (!isPlayer1sTurn)
+        {
+            player1Text.SetActive(false);
+            player2Text.SetActive(true);
+        }
     }
 
     private void ResetThrowState()
@@ -129,6 +183,7 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator BeginThrowReset()
     {
+        DisplayTextUserChange();
         yield return new WaitForSeconds(2f);
         ResetThrowState();
     }
